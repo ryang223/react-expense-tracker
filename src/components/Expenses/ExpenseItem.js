bfce29775b20cbaf3884ccb4cadea9f9dccc6327
ryang223 <ryangoodwd@gmail.com>
2022-02-21 11:42:23 -0500

    skeleton of update button configured along with basic DOM rendering of state change

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,28 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
     
+    const [title, setTitle] =  useState(props.title);
+
+        //the ES6 version of calling this function throws an unexpected token error so stick to 
+        //the ES5 variant for compilation's sake
+        function clickHandler() {
+        setTitle('Updated!');
+        console.log(title);
+    }; 
+
     return (
-        <Card className="expense-item">
+        <Card className='expense-item'>
            <ExpenseDate date={props.date} />
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
