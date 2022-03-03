c3bf9c23d7d32b3b72e12b1b75b8258ca514a1cd
ryang223 <ryangoodwd@gmail.com>
2022-03-03 14:35:30 -0500

    ExpensesList props items bug squashed and now compiles correctly

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css'; 
 
 const Expenses = (props) => {
@@ -18,20 +18,6 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear; 
       });
 
-
-      let expensesContent = <p>No expenses found.</p>
-
-      if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-          <ExpenseItem 
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-        />
-      ))
-    };
-
   return (
     <div>
       <Card className='expenses'>
@@ -39,10 +25,10 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
     );
   };
-  
-export default Expenses
+
+export default Expenses;
